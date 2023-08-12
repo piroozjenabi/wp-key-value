d01d2dfeb0e3671435dfb8c1c13e8fd234d0b158
pirooz jenabi <jenabi.pirooz@gmail.com>
2023-08-13 00:40:50 +0330

    chore fix search

--- view/pages/search_short_code.php
+++ view/pages/search_short_code.php
@@ -3,15 +3,19 @@ defined('ABSPATH') || exit;
 $keyVal = loadModel('KeyVal');
 $list = null;
 
-$data['tag']= $data['tag']??null;
-$data['search_text']= $data['search_text']??'Type Here ...';
-$data['search_button']= $data['search_button']??'search';
+$data['tag'] = $data['tag'] ?? null;
+$data['search_text'] = $data['search_text'] ?? 'Type Here ...';
+$data['search_button'] = $data['search_button'] ?? 'search';
 if (isset($_POST['search']) && $_POST['search']) {
     $value = $_POST['keyword'];
     if ($data['tag']) {
         $list = $keyVal->list(1, [
             't4.name' => $data['tag'],
             't3.value' => $value
+        ]);
+    }else{
+        $list = $keyVal->list(1, [
+            't1.val' => $data['tag'],
         ]);
     }
 }
@@ -31,25 +35,26 @@ if (isset($_POST['search']) && $_POST['search']) {
 
     </form>
 </div>
+<?php if (isset($_POST['search']) && $_POST['search']) :  ?>
+    <?php if ($list) : ?>
+        <table class="table is-bordered is-striped is-fullwidth is-narrow is-hoverable" >
+            <?php foreach ($list[10] as $key => $val) :
+                if ($key == 'id') continue;
+                if ($key == 'tags') continue;
 
-<?php if ($list) : ?>
-    <table>
-        <?php foreach ($list[10] as $key => $val) :
-            if ($key == 'id') continue;
-            if ($key == 'tags') continue;
+            ?>
+                <tr>
+                    <td><?= $key ?></td>
 
-        ?>
-            <tr>
-                <td><?= $key ?></td>
+                    <td><?= ($key == 'tags_print'
+                            ? implode(',', $val)
+                            : $val)
+                        ?></td>
+                </tr>
+            <?php endforeach; ?>
+        </table>
 
-                <td><?= ($key == 'tags_print'
-                        ? implode(',', $val)
-                        : $val)
-                    ?></td>
-            </tr>
-        <?php endforeach; ?>
-    </table>
-
-<?php else : ?>
-    <div class='notice'>data not found</div>
+    <?php else : ?>
+        <div class='notice'>data not found</div>
+    <?php endif; ?>
 <?php endif; ?>
